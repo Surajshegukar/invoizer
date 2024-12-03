5c82939fa9b559fb81a4ee1cc571933241cad138
Surajshegukar <surajshegukar2732@gmail.com>
2024-12-04 02:41:02 +0530

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 
 import Cookies from 'js-cookie';
 
-export const base_url = 'http://localhost:5000';
+// export const base_url = 'http://localhost:5000';
+export const base_url = 'https://invoizer-backend.vercel.app';
 
 export const token = Cookies.get('user');
