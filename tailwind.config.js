78febd26955078e8caa0a3b4a6039bb0ad3c70c5
Surajshegukar <surajshegukar2732@gmail.com>
2024-12-07 00:55:38 +0530

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// Note: This file is used to configure Tailwind CSS. You can learn more about it here: https://tailwindcss.com/docs/configuration
+
+import 'flowbite/plugin';
 
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
