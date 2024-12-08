4b4fb4a4adc75461c743a26745393023e292e0c5
Surajshegukar <surajshegukar2732@gmail.com>
2024-12-08 22:09:41 +0530

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // Note: This file is used to configure Tailwind CSS. You can learn more about it here: https://tailwindcss.com/docs/configuration
 
+/eslint-env node/
 
 module.exports = {
   serverModuleFormat: 'cjs',
